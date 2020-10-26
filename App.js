44a9bcf23a4b24e3a95a46e80220b06bab7e15f0
Perry <olsson.perry@gmail.com>
2020-10-25 18:01:04 -0700

    part 1

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+    return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello World yay</Text>
