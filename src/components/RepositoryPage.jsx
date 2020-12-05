01170c33657c7c54967af2b026e416312746d816
Perry Olsson <olsson.perry@gmail.com>
2020-12-05 11:22:58 -0800

    initial repository reviews display

--- src/components/RepositoryPage.jsx
+++ src/components/RepositoryPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableHighlight, View, StyleSheet } from 'react-native';
+import { TouchableHighlight, View, StyleSheet, FlatList } from 'react-native';
 import { useParams } from 'react-router-native';
 
 import RepositoryItem from './RepositoryItem';
@@ -11,18 +11,29 @@ const RepositoryPage = () => {
   const { id }= useParams();
   const { repository, loading } = useRepository(id);
 
-  const openGitHub = openLink(repository && repository.url);
-
   return loading ?
     <Text>loading...</Text>
     : (
-      <View>
-        <RepositoryItem repository={repository} />
-        <TouchableHighlight onPress={openGitHub} style={styles.touchable}>
-          <Text style={styles.text}>Open in Github</Text>
-        </TouchableHighlight>
-      </View>
+      <FlatList
+        data={repository.reviews.edges}
+        renderItem={({ item }) => <Text>{item.node.text}</Text>}
+        keyExtractor={({ id }) => id} 
+        ListHeaderComponent={() => <RepositoryInfo repository={repository} />}
+      />
     );
+};
+
+const RepositoryInfo = ({ repository }) => {
+  const openGitHub = openLink(repository && repository.url);
+  console.log(repository.reviews.edges);
+  return (
+    <View>
+      <RepositoryItem repository={repository} />
+      <TouchableHighlight onPress={openGitHub} style={styles.touchable}>
+        <Text style={styles.text}>Open in Github</Text>
+      </TouchableHighlight>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -32,6 +32,20 @@ export const GET_REPOSITORY = gql`
     repository(id: $id) {
       url
       ...RepositoryDetails
+      reviews {
+        edges {
+          node {
+            id
+            text
+            rating
+            createdAt
+            user {
+              id
+              username
+            }
+          }
+        }
+      }
     }
   }
   ${REPOSITORY_DETAILS}
