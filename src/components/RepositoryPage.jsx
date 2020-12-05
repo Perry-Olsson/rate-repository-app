dd3e8ca9de6a8bc3eb436e0fd885dea246d6c7c3
Perry Olsson <olsson.perry@gmail.com>
2020-12-05 11:01:08 -0800

    open github button

--- src/components/RepositoryPage.jsx
+++ src/components/RepositoryPage.jsx
@@ -5,21 +5,24 @@ import { useParams } from 'react-router-native';
 import RepositoryItem from './RepositoryItem';
 import useRepository from '../hooks/useRepository';
 import Text from '../components/Text';
+import { openLink } from '../utils';
 
 const RepositoryPage = () => {
   const { id }= useParams();
   const { repository, loading } = useRepository(id);
-  
-  if (loading) return <Text>loading...</Text>;
 
-  return (
-    <View>
-      <RepositoryItem repository={repository} />
-      <TouchableHighlight style={styles.touchable}>
-        <Text style={styles.text}>Open in Github</Text>
-      </TouchableHighlight>
-    </View>
-  );
+  const openGitHub = openLink(repository && repository.url);
+
+  return loading ?
+    <Text>loading...</Text>
+    : (
+      <View>
+        <RepositoryItem repository={repository} />
+        <TouchableHighlight onPress={openGitHub} style={styles.touchable}>
+          <Text style={styles.text}>Open in Github</Text>
+        </TouchableHighlight>
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+import { Linking } from "react-native";
+
+export const openLink = (url) => () => { Linking.openURL(url); };
