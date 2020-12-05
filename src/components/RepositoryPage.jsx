67f221eef0a6e2a298db11b869195cb30813ab1c
Perry Olsson <olsson.perry@gmail.com>
2020-12-04 17:19:49 -0800

    github link button

--- src/components/RepositoryPage.jsx
+++ src/components/RepositoryPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TouchableHighlight, View, StyleSheet } from 'react-native';
 import { useParams } from 'react-router-native';
 
 import RepositoryItem from './RepositoryItem';
 import useRepository from '../hooks/useRepository';
+import Text from '../components/Text';
 
 const RepositoryPage = () => {
   const { id }= useParams();
@@ -12,8 +13,27 @@ const RepositoryPage = () => {
   if (loading) return <Text>loading...</Text>;
 
   return (
-    <RepositoryItem repository={repository} />
+    <View>
+      <RepositoryItem repository={repository} />
+      <TouchableHighlight style={styles.touchable}>
+        <Text style={styles.text}>Open in Github</Text>
+      </TouchableHighlight>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  touchable: {
+    borderRadius: 4,
+    backgroundColor: '#006cf0',
+    margin: 10,
+  },
+  text: {
+    color: '#ffffff',
+    padding: 10,
+    fontSize: 20,
+    textAlign: 'center',
+  }
+});
 
 export default RepositoryPage;
